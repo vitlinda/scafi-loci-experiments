84f8ca7fdfaba727af0c6147a3bc88e68da8111a
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-07-05 16:43:14 +0200

    feat: use the local state in program evaluation

--- src/main/scala/it/unibo/loci/scafi/LogicalSystem.scala
+++ src/main/scala/it/unibo/loci/scafi/LogicalSystem.scala
@@ -1,6 +1,5 @@
 package it.unibo.loci.scafi
 
-
 import loci._
 import loci.communicator.tcp._
 import loci.language.Placement
@@ -21,33 +20,37 @@ import scala.util.Success
  */
 @multitier trait LogicalSystem { //defines only the component and the behaviour. The interaction is deployment-specific
   type SensorData = String // should be any or a wrap data class
-  type State = String // should be coherent with the aggregate computing context
+  type State = EXPORT // should be coherent with the aggregate computing context
   /**
    * Logical components used to define a device in a cyber-physical system.
    */
   // γ
   @peer type BehaviourComponent
-  def compute(id : ID, state : State, exports : Set[(ID, EXPORT)], sensors : Set[(LSNS, SensorData)]): (EXPORT, State) on BehaviourComponent = {
-    val sensorsMap = sensors.map { case (id, value) => (id, (value : Any)) }.toMap
-    val context = new ContextImpl(id, exports, sensorsMap, Map.empty) //todo
+  def compute(id: ID, state: State, exports: Set[(ID, EXPORT)], sensors: Set[(LSNS, SensorData)]): (
+      EXPORT,
+      State
+  ) on BehaviourComponent = {
+    val sensorsMap = sensors.map { case (id, value) => (id, (value: Any)) }.toMap
+    val context = new ContextImpl(id, exports + (id -> state), sensorsMap, Map.empty) //todo
     val program = new AggregateProgram { //make it trasmittable
-      override def main(): Any = foldhood(0)(_ + _)(1)
+      override def main(): Any = rep(0)(_ + 1) //foldhood(0)(_ + _)(1)
     }
-    (program.round(context), state)
+    val result = program.round(context)
+    (result, result)
   }
   // α
   @peer type ActuatorComponent
-  def actuation(id : ID, export : EXPORT) : Unit on ActuatorComponent = { println(s"id : $id --- export : ${export}")}
+  def actuation(id: ID, export: EXPORT): Unit on ActuatorComponent = println(s"id : $id --- export : $export")
   // σ
   @peer type SensorComponent
-  def sense(id : ID) : Set[(LSNS, SensorData)] on SensorComponent = Set.empty[(LSNS, SensorData)]
+  def sense(id: ID): Set[(LSNS, SensorData)] on SensorComponent = Set.empty[(LSNS, SensorData)]
   // k
   @peer type StateComponent
-  def state(id : ID) : State on StateComponent = on[StateComponent] { "empty" }
-  def update(id : ID, state : State) : Unit on StateComponent = on[StateComponent] { }
+  def state(id: ID): State on StateComponent = on[StateComponent](factory.emptyExport())
+  def update(id: ID, state: State): Unit on StateComponent = on[StateComponent] {}
   // x
   @peer type CommunicationComponent
-  def exports(id : ID) : Set[(ID, EXPORT)] on CommunicationComponent
+  def exports(id: ID): Set[(ID, EXPORT)] on CommunicationComponent
 }
 
 @multitier class P2PSystem extends LogicalSystem {
@@ -58,27 +61,29 @@ import scala.util.Success
   @peer type StateComponent <: Node
   @peer type CommunicationComponent <: Node
 
-  private val id : ID on Node = { UUID.randomUUID().hashCode() }
-  private val localExports : Var[Set[(ID, EXPORT)]] on Node = on[Node] { Var(Set.empty[(ID, EXPORT)]) }
-  def process(id : ID, export : EXPORT) : Unit on Node = placed {
+  private var _state: EXPORT on Node = factory.emptyExport()
+  private val id: ID on Node = UUID.randomUUID().hashCode()
+  private val localExports: Var[Set[(ID, EXPORT)]] on Node = on[Node](Var(Set.empty[(ID, EXPORT)]))
+  def process(id: ID, export: EXPORT): Unit on Node = placed {
     localExports.transform(exports => exports + (id -> export))
   }
 
+  override def state(id: ID): State on StateComponent = _state
+
+  override def update(id: ID, state: State): Unit on StateComponent = this._state = state
   override def exports(id: Int): Set[(ID, EXPORT)] on Node = localExports.now
 
-  def myState : State on Node = this.state(id)
-  def mySensors : Set[(LSNS, SensorData)] on Node = this.sense(id)
+  def myState: State on Node = this.state(id)
+  def mySensors: Set[(LSNS, SensorData)] on Node = this.sense(id)
 
-  def computeLocal(id : ID,
-                       state : State,
-                       exports : Set[(ID, EXPORT)],
-                       sensors : Set[(LSNS, SensorData)]): (EXPORT, State) on Node = {
-
+  def computeLocal(id: ID, state: State, exports: Set[(ID, EXPORT)], sensors: Set[(LSNS, SensorData)]): (
+      EXPORT,
+      State
+  ) on Node =
     super.compute(id, state, exports, sensors)
-  }
 
-  def main() : Unit on Node = {
-    while(true) {
+  def main(): Unit on Node = {
+    while (true) {
       val state = myState
       val myExports = exports(id)
       val sensors = mySensors
@@ -101,48 +106,48 @@ import scala.util.Success
   @peer type StateComponent <: Node
   @peer type CommunicationComponent <: Broker
 
-  private val id : ID on Node = { UUID.randomUUID().hashCode() }
-  private val brokerExport : Var[Set[(ID, EXPORT)]] on Broker = on[Broker] { Var(Set.empty[(ID, EXPORT)]) }
-  private def localExport(id : ID) : Future[Set[(ID, EXPORT)]] on Node = placed {
+  private var _state: EXPORT on Node = factory.emptyExport()
+  private val id: ID on Node = UUID.randomUUID().hashCode()
+  private val brokerExport: Var[Set[(ID, EXPORT)]] on Broker = on[Broker](Var(Set.empty[(ID, EXPORT)]))
+  private def localExport(id: ID): Future[Set[(ID, EXPORT)]] on Node = placed {
     remote[Broker].call(exportFixed(id)).asLocal
   }
-  def exportFixed(id: Int): Set[(ID, EXPORT)] on Broker = {
-    brokerExport.now.filter(_._1 != id)
-  }
-  //Has problems.. override doesn't work very well I suppose..
-  override def exports(id: Int): Set[(ID, EXPORT)] on Broker = {
-    brokerExport.now.filter(_._1 != id)
-  }
-  def process(id : ID, export : EXPORT) : Unit on Broker = brokerExport.transform(exports => exports + (id -> export))
-  def myState : State on Node = this.state(id)
-  def mySensors : Set[(LSNS, SensorData)] on Node = this.sense(id)
 
-  def computeLocal(id : ID,
-                   state : State,
-                   exports : Set[(ID, EXPORT)],
-                   sensors : Set[(LSNS, SensorData)]): (EXPORT, State) on Node = {
+  override def state(id: ID): State on StateComponent = _state
+
+  override def update(id: ID, state: State): Unit on StateComponent = this._state = state
+  def exportFixed(id: Int): Set[(ID, EXPORT)] on Broker =
+    brokerExport.now.filter(_._1 != id)
+  //Has problems.. override doesn't work very well I suppose..
+  override def exports(id: Int): Set[(ID, EXPORT)] on Broker =
+    brokerExport.now.filter(_._1 != id)
+  def process(id: ID, export: EXPORT): Unit on Broker = brokerExport.transform(exports => exports + (id -> export))
+  def myState: State on Node = this.state(id)
+  def mySensors: Set[(LSNS, SensorData)] on Node = this.sense(id)
+
+  def computeLocal(id: ID, state: State, exports: Set[(ID, EXPORT)], sensors: Set[(LSNS, SensorData)]): (
+      EXPORT,
+      State
+  ) on Node =
     super.compute(id, state, exports, sensors)
-  }
   import scala.concurrent.ExecutionContext.Implicits.global
-  def main() : Unit on Root = (on[Node] {
+  def main(): Unit on Root = on[Node] {
     println("Device starts ...")
     logic()
-  }) and (on[Broker] {
+  } and on[Broker] {
     println("Broker starts ...")
-  })
+  }
 
-  def logic() : Future[Unit] on Node = {
+  def logic(): Future[Unit] on Node = {
     val futureExport = localExport(id)
-    futureExport.flatMap {
-      export => {
-        val sensors = mySensors
-        val state = myState
-        val result = computeLocal(id, state, export, sensors)
-        update(id, result._2)
-        actuation(id, result._1)
-        remote.call(process(id, result._1)).asLocal
-      }
-    }.flatMap { a => Future { Thread.sleep(1000) } }.flatMap(_ => logic())
+    futureExport.flatMap { export =>
+      val sensors = mySensors
+      val state = myState
+      val result = computeLocal(id, state, export, sensors)
+      update(id, result._2)
+      actuation(id, result._1)
+      remote.call(process(id, result._1)).asLocal
+    }.flatMap(a => Future(Thread.sleep(1000))).flatMap(_ => logic())
   }
 }
 
@@ -151,23 +156,33 @@ import scala.util.Success
 
 object SystemPeerTest extends App {
   val port = 3245
-  multitier.start(new Instance[SimpleExampleP2P.Node](
-    connect[SimpleExampleP2P.Node](TCP(port).firstConnection)
-  ))
-  multitier.start(new Instance[SimpleExampleP2P.Node](
-    connect[SimpleExampleP2P.Node](TCP("localhost", port))
-  ))
+  multitier.start(
+    new Instance[SimpleExampleP2P.Node](
+      connect[SimpleExampleP2P.Node](TCP(port).firstConnection)
+    )
+  )
+  multitier.start(
+    new Instance[SimpleExampleP2P.Node](
+      connect[SimpleExampleP2P.Node](TCP("localhost", port))
+    )
+  )
 }
 
 object SystemBrokerTest extends App {
   val port = 3245
-  multitier.start(new Instance[SimpleExampleBroker.Broker](
-    listen[SimpleExampleBroker.Node](TCP(port))
-  ))
-  multitier.start(new Instance[SimpleExampleBroker.Node](
-    connect[SimpleExampleBroker.Broker](TCP("localhost", port))
-  ))
-  multitier.start(new Instance[SimpleExampleBroker.Node](
-    connect[SimpleExampleBroker.Broker](TCP("localhost", port))
-  ))
+  multitier.start(
+    new Instance[SimpleExampleBroker.Broker](
+      listen[SimpleExampleBroker.Node](TCP(port))
+    )
+  )
+  multitier.start(
+    new Instance[SimpleExampleBroker.Node](
+      connect[SimpleExampleBroker.Broker](TCP("localhost", port))
+    )
+  )
+  multitier.start(
+    new Instance[SimpleExampleBroker.Node](
+      connect[SimpleExampleBroker.Broker](TCP("localhost", port))
+    )
+  )
 }
